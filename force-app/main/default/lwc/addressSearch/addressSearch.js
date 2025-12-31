4aa8b6e0f2271017ca7333833537b31f69f1313a
Julio Cesar <jcmonzem@hotmail.com>
2025-12-31 00:00:00 -0300

    Created LWC for Search Address

---
+++ force-app/main/default/lwc/addressSearch/addressSearch.js
@@ -0,0 +1,22 @@
+import { LightningElement } from 'lwc';
+import searchAndSave from '@salesforce/apex/AddressService.searchAndSave';
+
+export default class AddressSearch extends LightningElement {
+    cep;
+    @track address;
+
+    handleChange(event) {
+        this.cep = event.target.value;
+    }
+
+    handleSearch() {
+        searchAndSave({ cep: this.cep })
+            .then(result => {
+                this.address = result;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+}
